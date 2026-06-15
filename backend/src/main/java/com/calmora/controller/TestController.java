44c34f0b8bf13767f9bb38e453887ddba1f8505a
manis <manishi23nigam06@gmail.com>
2026-06-15 13:37:49 +0530

    added test controller

--- backend/src/main/java/com/calmora/controller/TestController.java
+++ backend/src/main/java/com/calmora/controller/TestController.java
@@ -2,12 +2,14 @@ package com.calmora.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
-@RestController("/api/test")
+@RestController
+@RequestMapping("/api/test")
 public class TestController {
 
     @GetMapping("/health")
