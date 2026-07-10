7685fb76be33ea4bd9ddf8734527fb907b93f73e
manis <manishi23nigam06@gmail.com>
2026-07-10 15:21:23 +0530

    payment

---
+++ backend/src/main/java/com/calmora/model/Enum/status.java
@@ -0,0 +1,9 @@
+package com.calmora.model.Enum;
+
+public enum status {
+    PENDING,
+    SUCCESS,
+    FAILED,
+    REFUNDED
+}
+
---
+++ backend/src/main/java/com/calmora/model/Payment.java
@@ -0,0 +1,56 @@
+package com.calmora.model;
+
+import com.calmora.model.Enum.status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "payment")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id")
+    private String orderId;
+
+    @Column(name = "razorpay_order_id")
+    private String razorpayOrderId;
+
+    private String razorpayPaymentId;
+    private String razorpaySignature;
+
+    @Enumerated(EnumType.STRING)
+    private status status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+    private String currency;
+    private int appointmentId;
+    private Long therapistId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
